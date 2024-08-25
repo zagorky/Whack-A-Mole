93c3b863c560392d43e058727b9dd8923bf5ee1f
zagorky <zagorkiynahyi@gmail.com>
2024-08-25 18:38:47 +0300

    feat: doing burder on the pets page

--- shelter/scripts/index.js
+++ shelter/scripts/index.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-links a");
   const adaptive = document.getElementById("adaptive");
   const overlay = document.getElementById("overlay");
+  const header = document.getElementById("header");
+  const wrapperPets = document.getElementById("wrapper-header");
+  const burgerIcon = document.querySelectorAll(".burger-line");
+  let url = window.location.href;
 
   function toggleMenu() {
     burgerMenu.classList.toggle("open");
@@ -18,6 +22,24 @@ document.addEventListener("DOMContentLoaded", function () {
   navLinks.forEach((link) => {
     link.addEventListener("click", toggleMenu);
   });
+  if (url.includes("pets.html")) {
+    header.classList.toggle("pets-page");
+    burgerIcon.forEach((i) => {
+      i.classList.toggle("dark");
+    });
+    if (window.screen.width >= 768) {
+      navLinks.forEach((link) => {
+        link.classList.toggle("pets-page");
+      });
+    }
+    // if (this.classList.includes("open")) {
+    //   burgerIcon.forEach((elem) => {
+    //     elem.style.background = "#F1CDB3";
+    //   });
+    // }
+
+    wrapperPets.classList.toggle("pets-page");
+  }
 });
 
 //пагинация
