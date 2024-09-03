cd52b5d5144e11c5f4105d7e12b4d7068a1d94df
zagorky <zagorkiynahyi@gmail.com>
2024-09-03 12:10:33 +0300

    feat: start doing popup

--- shelter/scripts/index.js
+++ shelter/scripts/index.js
@@ -16,9 +16,32 @@ fetch("./data/pets.json")
       card.innerHTML = `
                             <img src="${pet.img}" alt="our-friends-pic" />
                             <p>${pet.name}</p>
-                            <button>Learn more</button>
+                            <button class='btn'>Learn more</button>
               `;
       cardsContainer.appendChild(card);
+      const btn = card.querySelector("btn");
+      const popupContainer = document.createElement("div");
+      popupContainer.classList.add("popup-cont", "hidden");
+      popupContainer.innerHTML = `
+            <div class='popup-content'>
+            <h3>${pet.name}</h3>
+            <h4>${pet.type} - ${pet.breed}</h4>
+            <p>${pet.description}</p>
+            <ul>
+              <li><b>Age:</b>${pet.age}</li>
+              <li><b>Inoculations:</b>${pet.inoculations}</li>
+              <li><b>Diseases:</b>${pet.diseases}</li>
+              <li><b>Parasites:</b>${pet.parasites}</li>
+            </ul> 
+            </div>`;
+      document.body.appendChild(popupContainer);
+      card.addEventListener("click", () => {
+        popupContainer.classList.toggle("hidden");
+      });
+      popupContainer.addEventListener("click", (event) => {
+        if (event.target.classList.contains("popup-cont"))
+          popupContainer.classList.toggle("hidden");
+      });
     });
   })
   .catch((error) => console.error("Ошибка при исполнении запроса: ", error));
