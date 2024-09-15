b2d8abc4bc3b9a2cb8d1b29acf4605f76d99247f
zagorky <zagorkiynahyi@gmail.com>
2024-09-15 12:14:00 +0300

    init: add files and start doing task

--- js30custom-video/scripts/index.js
+++ js30custom-video/scripts/index.js
@@ -1,1 +1,19 @@
 console.log("самопроверка");
+
+//
+
+const video = new Video();
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+
+function playVideo() {
+  video.src =
+    "https://github.com/rolling-scopes-school/zagorky-JSFEPRESCHOOL2024Q2/blob/js30%231.3-custom-video/js30custom-video/assets/video/video.mp4";
+  video.currentTime = 0;
+  video.play();
+}
+function pauseVideo() {
+  video.pause();
+}
+play.addEventListener("click", playVideo);
+pause.addEventListener("click", pauseVideo);
