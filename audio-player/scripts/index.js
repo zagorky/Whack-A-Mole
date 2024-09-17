9de0069a688306ac80c5b01e5aec92818377198f
zagorky <zagorkiynahyi@gmail.com>
2024-09-17 13:36:08 +0300

    feat: fix volume input logic and add autoplay

--- audio-player/scripts/index.js
+++ audio-player/scripts/index.js
@@ -2,10 +2,13 @@ import { audioData } from "../assets/data/data.js";
 
 const player = document.querySelector(".player");
 const audio = player.querySelector("audio");
-const playPauseBtn = player.querySelector(".play"); //кнопка play/pause
+const playPauseBtn = player.querySelector(".play");
 const progressBar = document.querySelector(".progress-filled");
 const curTime = document.querySelector(".currentTime");
 const dur = document.querySelector(".duration");
+const volume = player.querySelector(".volume");
+const soundMuteBtn = player.querySelector(".sound");
+const shuffle = document.querySelector(".shuffle");
 
 let progression;
 let currentTrack = 0;
@@ -28,6 +31,11 @@ function playAudio() {
     updateProgress();
   }
 }
+function autoPlay() {
+  if (audio.ended) {
+    nextTrack();
+  }
+}
 function updateProgress() {
   let prog = (audio.currentTime / audio.duration) * 100;
   progressBar.value = prog;
@@ -36,9 +44,9 @@ function updateProgress() {
     playPauseBtn.classList.remove("pause");
     playPauseBtn.classList.add("play");
     clearInterval(progression);
+    autoPlay();
   }
 }
-
 function getTrack(index) {
   const song = audioData[index];
   audio.src = song.src;
@@ -52,17 +60,46 @@ function nextTrack() {
   getTrack(currentTrack);
   playAudio();
 }
-
 function prevTrack() {
   currentTrack = (currentTrack - 1 + audioData.length) % audioData.length;
   getTrack(currentTrack);
   playAudio();
 }
-
 function formatTime(seconds) {
   const min = Math.floor(seconds / 60);
   const sec = Math.floor(seconds % 60);
   return `${min}:${sec < 10 ? "0" + sec : sec}`;
+}
+function updateVolume(event) {
+  let volumeLvl = event.target.value;
+  audio.volume = volumeLvl;
+
+  if (audio.volume > 0) {
+    audio.muted = false;
+    soundMuteBtn.classList.remove("mute");
+    soundMuteBtn.classList.add("sound");
+  } else {
+    audio.muted = true;
+    soundMuteBtn.classList.add("mute");
+    soundMuteBtn.classList.remove("sound");
+  }
+}
+function handleMute() {
+  audio.muted = !audio.muted;
+  if (audio.muted) {
+    soundMuteBtn.classList.add("mute");
+    soundMuteBtn.classList.remove("sound");
+  } else {
+    soundMuteBtn.classList.remove("mute");
+    soundMuteBtn.classList.add("sound");
+    if (audio.volume === 0) {
+      audio.volume = 0.5;
+      volume.value = 0.5;
+    }
+  }
+}
+function shuffleTracks() {
+  [];
 }
 
 playPauseBtn.addEventListener("click", playAudio);
@@ -77,3 +114,5 @@ progressBar.addEventListener("input", () => {
   audio.currentTime = newCurTime;
   updateProgress();
 });
+soundMuteBtn.addEventListener("click", handleMute);
+volume.addEventListener("change", updateVolume);
