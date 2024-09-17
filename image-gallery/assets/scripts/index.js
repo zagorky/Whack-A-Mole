606cea36609bdf73b7dd316195076b3cc42bc00e
zagorky <zagorkiynahyi@gmail.com>
2024-09-17 20:13:19 +0300

    feat: search is done but not well

--- image-gallery/assets/scripts/index.js
+++ image-gallery/assets/scripts/index.js
@@ -2,7 +2,8 @@ const searchBox = document.querySelector(".search-box input");
 const searchBtn = document.querySelector(".search");
 let searchingFor = "";
 const content = document.querySelector(".content");
-let url = `https://api.unsplash.com/photos/random?query=${searchingFor}&count=6&orientation=landscape&client_id=fg3V91GfUO8v970NVSOQw1IFhoi9XfHHoaTSN7sCZQE`;
+
+let url = `https://api.unsplash.com/photos/random?query=&count=6&orientation=landscape&client_id=fg3V91GfUO8v970NVSOQw1IFhoi9XfHHoaTSN7sCZQE`;
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
@@ -11,7 +12,7 @@ async function getData() {
 getData();
 
 function createImg(data) {
-  const image = content.createElement("img");
+  const image = document.createElement("img");
   image.classList.add("img");
   image.src = `${data.urls.regular}`;
   image.alt = `${data.alt_description}`;
@@ -26,9 +27,17 @@ function infinityScroll() {
     getData();
   }
 }
-searchBtn.addEventListener("click", () => {
-  searchingFor = searchBox.value.trim();
-  console.log(searchingFor);
-  if (searchingFor) getData();
-});
+function goSearching() {
+  searchingFor = searchBox.value.trim().toString();
+  url = `https://api.unsplash.com/photos/random?query=${searchingFor}&count=6&orientation=landscape&client_id=fg3V91GfUO8v970NVSOQw1IFhoi9XfHHoaTSN7sCZQE`;
+  getData();
+}
+
 // window.addEventListener("scroll", infinityScroll);
+searchBtn.addEventListener("click", goSearching);
+searchBtn.addEventListener("keypress", (event) => {
+  let key = event.witch || event.keyCode;
+  if (key === 13) {
+    searchBtn.click();
+  }
+});
