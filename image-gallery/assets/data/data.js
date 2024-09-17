4dcd10b24799997e27032d513d7b6e113a91f516
zagorky <zagorkiynahyi@gmail.com>
2024-09-17 19:36:08 +0300

    feat: add scroll and working on the search

--- image-gallery/assets/data/data.js
+++
@@ -1,8 +0,0 @@
-const url = `https://api.unsplash.com/photos/random?query=${searchingFor}&client_id=`;
-
-async function getData() {
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
-}
-getData();
--- image-gallery/assets/scripts/index.js
+++ image-gallery/assets/scripts/index.js
@@ -1,12 +1,34 @@
-// import data from "./assets/data/data.js";
-
-const searchingFor = document.querySelector("input");
+const searchBox = document.querySelector(".search-box input");
+const searchBtn = document.querySelector(".search");
+let searchingFor = "";
 const content = document.querySelector(".content");
-const url = `https://api.unsplash.com/photos/random?query=${searchingFor.value}&client_id=fg3V91GfUO8v970NVSOQw1IFhoi9XfHHoaTSN7sCZQE`;
-
+let url = `https://api.unsplash.com/photos/random?query=${searchingFor}&count=6&orientation=landscape&client_id=fg3V91GfUO8v970NVSOQw1IFhoi9XfHHoaTSN7sCZQE`;
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  data.forEach((el) => createImg(el));
 }
 getData();
+
+function createImg(data) {
+  const image = content.createElement("img");
+  image.classList.add("img");
+  image.src = `${data.urls.regular}`;
+  image.alt = `${data.alt_description}`;
+  content.append(image);
+}
+
+function infinityScroll() {
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+  const scrollPos = window.scrollY;
+  if (documentHeight - (windowHeight + scrollPos) <= 100) {
+    getData();
+  }
+}
+searchBtn.addEventListener("click", () => {
+  searchingFor = searchBox.value.trim();
+  console.log(searchingFor);
+  if (searchingFor) getData();
+});
+// window.addEventListener("scroll", infinityScroll);
