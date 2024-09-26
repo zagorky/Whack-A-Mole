8e8df014cf6d9d9fb9c47c1309cbb85a93d62857
zagorky <zagorkiynahyi@gmail.com>
2024-09-26 19:54:16 +0300

    feat: add scoreBoard and try to do timer

--- random-game/assets/scripts/index.js
+++ random-game/assets/scripts/index.js
@@ -1,8 +1,14 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+const startBtn = document.querySelector(".start");
+const scoreList = document.querySelector(".scoreList");
+const timerCont = document.querySelector(".timer");
 
 let lastHole;
+let timeUp = false;
+let score = 0;
+let lastTime = 10;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) * min);
@@ -20,13 +26,56 @@ function randomHole(holes) {
 
 function upDown() {
   const time = randomTime(20, 100);
-  console.log(time);
   const hole = randomHole(holes);
   hole.classList.add("up");
   setTimeout(() => {
     hole.classList.remove("up");
-    upDown();
+    if (!timeUp) {
+      upDown();
+    }
   }, time);
 }
 
-upDown();
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  upDown();
+  setTimeout(() => {
+    timeUp = true;
+    addScore(score);
+  }, 10000);
+  setInterval(timer(lastTime), 1000);
+}
+
+function bonk(event) {
+  if (!event.isTrusted) return;
+  score++;
+  this.classList.remove("up");
+  scoreBoard.textContent = `Score: ${score}`;
+}
+
+function timer(lastTime) {
+  if (lastTime == 0) {
+    return (timerCont.textContent = `Time: ${lastTime}`);
+  } else {
+    console.log(lastTime);
+    return (timerCont.textContent = `Time: ${lastTime * timer(lastTime - 1)}`);
+  }
+}
+function addScore(score) {
+  window.localStorage.setItem("Score", score);
+  const ul = document.createElement("ul");
+  scoreList.appendChild(ul);
+  const li = document.createElement("li");
+  li.classList.add("scoreItem");
+  li.textContent = `Score: ${window.localStorage.getItem("Score")}`;
+  ul.appendChild(li);
+}
+
+// function resetScore() {
+//   if
+// }
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
+startBtn.addEventListener("click", startGame);
